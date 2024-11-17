57610af44da1ca34483000a183f639244f4324d0
Mixel-MurP-427 <connor.mullarky@outlook.com>
2024-11-17 12:51:53 -0800

    finished here

--- Nerf_Trigger.py
+++ Nerf_Trigger.py
@@ -1,22 +1,29 @@
 print('program start')
 from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor
+from pybricks.pupdevices import Motor, Remote
 from pybricks.parameters import Stop, Port, Color, Button
 from pybricks.tools import wait, run_task, multitask
 
 hub = PrimeHub(observe_channels=[198])
-#TODO read remote control
+hub.speaker.volume(100)
+#myRemote = Remote(name='Nintendo')
+
 
 motorA = Motor(Port.A, reset_angle=True, profile=5) #with sticker
 motorB = Motor(Port.B, reset_angle=True, profile=5)
-print(motorB.angle())
-speed = 200
+speed = 1000
 turn_angle = 180
 
+def ready():
+    while hub.ble.observe(198) == None:
+        wait(10)
 
 def launch_signal():
-    while not Button.BLUETOOTH in hub.buttons.pressed():
+
+    print(hub.buttons.pressed())
+    while hub.ble.observe(198) != True:
         wait(10)
+
 
 def calibrate():
 
@@ -28,12 +35,18 @@ def calibrate():
 
 
 
+
+#main code
+hub.light.on(Color.ORANGE)
 calibrate()
+ready()
+hub.light.blink(Color.GREEN, (30, 1970))
 launch_signal()
 #fire
+hub.light.blink(Color.RED, (100, 100))
 motorA.run_angle(speed, -turn_angle, then=Stop.BRAKE, wait=False)
 motorB.run_angle(speed, turn_angle, then=Stop.BRAKE, wait=True)
-wait(2000)
+hub.speaker.beep(1000, 3000)
 calibrate()
 
 
